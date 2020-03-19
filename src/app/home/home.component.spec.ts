9db6fa87fb3e7af10ae5e680c0e2ad1855d1541e
Jason B <jason@browne.me.uk>
2020-03-19 23:03:09 +0000

    Provide content for home component

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,25 +1,47 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { PageObjectBase } from '../lib/testing/page-object.base';
 import { HomeComponent } from './home.component';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
+  let pageObject: HomeComponentPageObject;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      declarations: [HomeComponent]
     })
-    .compileComponents();
+      .compileComponents()
+      .then(() => {
+        fixture = TestBed.createComponent(HomeComponent);
+        component = fixture.componentInstance;
+        pageObject = new HomeComponentPageObject(fixture);
+        fixture.detectChanges();
+      });
   }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(HomeComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have backdrop url with root path', () => {
+    const backgroundImage = pageObject.backdrop.style.backgroundImage;
+    expect(backgroundImage.startsWith(`url("${component.rootPath}`)).toBeTrue();
+  });
+
+  it('should calculate age of layout', () => {
+    expect(component.ageLayout).toBeGreaterThan(0);
+    expect(pageObject.taglineText).toContain(`${component.ageLayout} years`);
+  });
 });
+
+class HomeComponentPageObject extends PageObjectBase<HomeComponent> {
+  public get backdrop(): HTMLDivElement {
+    return this.select('[data-backdrop]');
+  }
+
+  public get taglineText(): string {
+    return this.select('[data-tagline]').textContent;
+  }
+}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { ROOT_PATH } from 'src/variables';
+
+import { Component, Inject, Optional } from '@angular/core';
 
 @Component({
   selector: 'gvr-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
+  public rootPath: string;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(@Optional() @Inject(ROOT_PATH) rootPath: string) {
+    this.rootPath = rootPath || '/';
   }
 
+  public get ageLayout(): number {
+    return new Date().getFullYear() - 1995;
+  }
 }
