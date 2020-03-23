dd2313c1d1e9e2683589b4e5979db9fa7dae891b
Jason B <jason@browne.me.uk>
2020-03-23 21:46:40 +0000

    Add pipe to convert values to human-readable format

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,9 +4,15 @@ import { NgModule } from '@angular/core';
 import { DataExtractionPipe } from './data-extraction.pipe';
 import { DataFilterPipe } from './data-filter.pipe';
 import { FormatKeyPipe } from './format-key.pipe';
+import { FormatValuePipe } from './format-value.pipe';
 
 @NgModule({
-  declarations: [DataExtractionPipe, DataFilterPipe, FormatKeyPipe],
+  declarations: [
+    DataExtractionPipe,
+    DataFilterPipe,
+    FormatKeyPipe,
+    FormatValuePipe
+  ],
   imports: [CommonModule]
 })
 export class SharedModule {}
