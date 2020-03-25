d709f328f10db0f730a8493e6e81e240fd6fb074
Jason B <jason@browne.me.uk>
2020-03-25 22:25:26 +0000

    Implement layout summary

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { ErrorComponent } from './error/error.component';
 import { HomeComponent } from './home/home.component';
+import { LayoutComponent } from './layout/layout.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
+  { path: 'layout', component: LayoutComponent },
   { path: '**', component: ErrorComponent }
 ];
 
