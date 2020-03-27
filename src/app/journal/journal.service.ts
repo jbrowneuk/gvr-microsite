8b43bc13b665e7437de36663c7ac4d00c7da2d4c
Jason B <jason@browne.me.uk>
2020-03-27 20:27:29 +0000

    Add journal service

---
+++ src/app/journal/journal.service.ts
@@ -0,0 +1,27 @@
+import { Observable } from 'rxjs';
+
+import { HttpClient } from '@angular/common/http';
+import { Inject, Injectable, Optional } from '@angular/core';
+
+import { ROOT_PATH } from '../variables';
+import { PostDataWrapper } from './model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class JournalService {
+  protected rootPath: string;
+
+  constructor(
+    private http: HttpClient,
+    @Optional() @Inject(ROOT_PATH) rootPath: string
+  ) {
+    this.rootPath = rootPath || '/';
+  }
+
+  public fetchPosts(page: number): Observable<PostDataWrapper> {
+    return this.http.get<PostDataWrapper>(
+      `${this.rootPath}api/?posts&page=${page || 1}`
+    );
+  }
+}
