dc2e7e0292a322ce4428dfdfca2966fff2f9be6c
Jason B <jason@browne.me.uk>
2020-04-23 23:42:32 +0100

    Link part list to statistics

--- src/app/part-browser/part-statistics/part-statistics.component.spec.ts
+++ src/app/part-browser/part-statistics/part-statistics.component.spec.ts
@@ -4,6 +4,7 @@ import { PartCategory, PartList } from 'src/app/model';
 import { IMock, Mock } from 'typemoq';
 
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { PartBrowserFacade } from '../state/part-browser.facade';
 import { PartStatisticsComponent } from './part-statistics.component';
@@ -84,6 +85,7 @@ describe('Part Statistics Component', () => {
 
     await TestBed.configureTestingModule({
       declarations: [PartStatisticsComponent],
+      imports: [RouterTestingModule],
       providers: [
         { provide: PartBrowserFacade, useFactory: () => mockFacade.object }
       ]
